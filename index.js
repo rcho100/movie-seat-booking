e5ecd7aa07b406e561c7ec66dcfcf19d607db404
Richard Cho <richardcho100@gmail.com>
2020-08-25 23:58:00 -0400

    Save selected show index & price

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const seatingArea = document.querySelector('.seating-area')
+const seats = document.querySelectorAll('.row .seat:not(.occupied)')
 const showSelect = document.querySelector('.show')
 const count = document.getElementById('count')
 const total = document.getElementById('total')
 
 let ticketPrice = +showSelect.value
 
+//Save selected show index & price
+const setShowData = (showIndex, showPrice) => {
+    localStorage.setItem('selectedShowIndex', showIndex)
+    localStorage.setItem('selectedShowPrice', showPrice)
+}
+
 //Update total and count
 const updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex ))
     const selectedSeatsCount = selectedSeats.length
 
     count.innerText = selectedSeatsCount
@@ -17,6 +28,7 @@ const updateSelectedCount = () => {
 //Show select event
 showSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value
+    setShowData(e.target.selectedIndex, e.target.value)
     updateSelectedCount()
 })
 
